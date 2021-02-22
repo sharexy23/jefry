a4f0821daeff319bb12202a257e18dab16a27ee3
sharexy23 <omotietie@yahoo.com>
2021-02-22 14:22:31 +0100

    FGGF

--- resources/user.py
+++ resources/user.py
@@ -264,7 +264,7 @@ class TransferHistory(Resource):
                         )
 
     #it seems to me that this function is cursed
-    @jwt_required
+    @jwt_required()
     def post(self):
         data = TransferHistory.parser.parse_args()
         user = Ujer.find_by_phone_number(data['phone_number'])
